476ec848e8e437889f2ed1ea09359fd7b4002d25
wty92911 <2066946886@qq.com>
2024-01-14 22:21:13 +0800

    past save

--- web/src/views/autocare/saleOrder/detailPage/api.js
+++ web/src/views/autocare/saleOrder/detailPage/api.js
@@ -1,25 +1,25 @@
 import { GetList as getVehicles } from '@/views/autocare/vehicle/api'
-import { GetList as getVehicleParts} from '@/views/autocare/vehiclePart/api'
-import { GetList as getEmployees} from '@/views/autocare/employee/api'
-import { GetList as getOrderPart, DelObj as delOrderPart, UpdateObj as updateOrderPart, AddObj as addOrderPart} from '@/views/autocare/saleOrderPart/api'
+import { GetList as getVehicleParts } from '@/views/autocare/vehiclePart/api'
+import { GetList as getEmployees } from '@/views/autocare/employee/api'
+import { GetList as getOrderPart, DelObj as delOrderPart, UpdateObj as updateOrderPart, AddObj as addOrderPart } from '@/views/autocare/saleOrderPart/api'
 export function GetVehicles(query) {
-    return getVehicles(query)
+  return getVehicles(query)
 }
 export function GetVehicleParts(query) {
-    return getVehicleParts(query)
+  return getVehicleParts(query)
 }
 export function GetEmployees(query) {
-    return getEmployees(query)
+  return getEmployees(query)
 }
 export function GetOrderPart(query) {
-    return getOrderPart(query);
+  return getOrderPart(query)
 }
 export function DelOrderPart(query) {
-    return delOrderPart(query);
+  return delOrderPart(query)
 }
 export function UpdateOrderPart(query) {
-    return updateOrderPart(query);
+  return updateOrderPart(query)
 }
 export function AddOrderPart(query) {
-    return addOrderPart(query);
+  return addOrderPart(query)
 }
